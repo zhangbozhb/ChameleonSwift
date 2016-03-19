9af9b64621f74173d86d412a7bc07560b6893a71
travel <travel@thecover.cn>
2016-03-19 15:25:04 +0800

    Initial Commit

---
+++ Chameleon/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Chameleon
+//
+//  Created by travel on 16/3/19.
+//  Copyright © 2016年 travel. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ ChameleonTests/ChameleonTests.swift
@@ -0,0 +1,36 @@
+//
+//  ChameleonTests.swift
+//  ChameleonTests
+//
+//  Created by travel on 16/3/19.
+//  Copyright © 2016年 travel. All rights reserved.
+//
+
+import XCTest
+@testable import Chameleon
+
+class ChameleonTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
